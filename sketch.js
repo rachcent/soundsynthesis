8fd330ed5dfa59548f49d090022b5a70cb744ed1
rachcent <35813484+rachcent@users.noreply.github.com>
2019-11-08 16:28:34 -0800

    q

--- sketch.js
+++ sketch.js
@@ -1,46 +1,58 @@
-let angle = .5;
-let x = 200
-let y = 200
+let angle = 1;
+let x = 400;
+let y = 400;
 
 
 let song;
 
 function preload() {
-  song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
+  song = loadSound('tchami.mp3');
 }
 
 function setup() {
-  createCanvas(710, 200);
-  song.loop(); // song is ready to play during setup() because it was loaded during preload
-  background(0, 255, 0);
+  createCanvas(800, 800);
+  amplitude = new p5.Amplitude();
 }
 
 function mousePressed() {
   if (song.isPlaying()) {
     // .isPlaying() returns a boolean
     song.pause(); // .play() will resume from .pause() position
-    background(255, 0, 0);
   } else {
     song.play();
-    background(0, 255, 0);
   }
 }
 
 function draw() {
   background(0);
-  
- 
+fill(255);
+let level = amplitude.getLevel();
+let size = map(level, 0, 1, 0, 200);
+let sizecircle= map(level, 0, 1, 0, 1000);
+// ellipse(width/2, height/2, size, size);
+var d = random(0, 255);
+var e = random(0, 255);
+var f = random(0, 255);
+
+noFill();
+stroke(d,e,f)
+ellipse(x, y, sizecircle, sizecircle);
+
   translate(x,y);
+  fill(d,e,f);
   rotate(angle);
   rectMode(CENTER);
-  rect(0,0,50,50);
-  
-  translate(50,50)
-  fill(255,100,50)
-  rect(0, 0, 20, 20);
-  
+  rect(0,0, size, size);
+
+  translate(size, size );
+  fill(d,e,f);
+  ellipse(0, 0, size, size);
+  translate(100, 100 );
+  rect(level, level, 20, 20);
+
+
   //moves across the screen
-  // x = x + 1;
+  x = x
   //rotates square
-  angle = angle + 1;
+  angle = angle + size;
 }
