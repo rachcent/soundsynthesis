c45c942b5e6526fb8ed1ed868990c5213e782445
rachcent <35813484+rachcent@users.noreply.github.com>
2019-11-11 11:07:00 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let angle = 1;
 let x = 400;
 let y = 400;
+var fft = new p5.FFT();
 
 
 let song;
@@ -12,6 +13,7 @@ function preload() {
 function setup() {
   createCanvas(800, 800);
   amplitude = new p5.Amplitude();
+  fft = new p5.FFT();
 }
 
 function mousePressed() {
@@ -28,31 +30,51 @@ function draw() {
 fill(255);
 let level = amplitude.getLevel();
 let size = map(level, 0, 1, 0, 200);
-let sizecircle= map(level, 0, 1, 0, 1000);
+
+// Scale the level value from 1 to 800 to a range between 0 and 175
+let sizecircle= map(level, 0, 1, 0, 700);
+
 // ellipse(width/2, height/2, size, size);
 var d = random(0, 255);
 var e = random(0, 255);
 var f = random(0, 255);
 
-noFill();
-stroke(d,e,f)
-ellipse(x, y, sizecircle, sizecircle);
+  //muiltiple suns
+	for (var x = 5; x < 800; x = x+50){
+		for (var y = 5; y < 800; y = y+50){
+			push();
+			translate(x, y);
+      rotate(mouseY);
+	  	drawSun();
+			pop();
+		}
+  }
 
+function drawSun(){
+  //bass circles
+  noFill();
+  strokeWeight(2);
+  stroke(d,e,f)
+  ellipse(x, y, sizecircle, sizecircle);
+
+  //rectangle in the center
   translate(x,y);
   fill(d,e,f);
   rotate(angle);
   rectMode(CENTER);
   rect(0,0, size, size);
 
+  //circles around the square
   translate(size, size );
   fill(d,e,f);
   ellipse(0, 0, size, size);
   translate(100, 100 );
-  rect(level, level, 20, 20);
+  rect(0, 0, 20, 20);
 
 
   //moves across the screen
   x = x
   //rotates square
   angle = angle + size;
+}
 }
